04c88045df171acc829d18ccc77be0b9e0d03a6e
nkwaaaa <nkwaaaa@gmail.com>
2021-10-16 19:04:47 -0300

    Agregado íconos

--- app/navigations/Navigation.js
+++ app/navigations/Navigation.js
@@ -2,6 +2,8 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
+import { Icon } from "react-native-elements";
+
 import RestaurantsStack from "./RestaurantsStack";
 import FavoritesStack from "./FavoritesStack";
 import TopRestaurantsStack from "./TopRestaurantsStack";
@@ -13,7 +15,16 @@ const Tab = createBottomTabNavigator();
 export default function Navigation() {
     return (
         <NavigationContainer>
-            <Tab.Navigator>
+            <Tab.Navigator
+                initialRouteName="restaurants"
+                tabBarOptions={{
+                    inactiveTintColor: "gray",
+                    activeTintColor: "blue",
+                }}
+                screenOptions={({ route }) => ({
+                    tabBarIcon: ({ color }) => screenOptions(route, color),
+                })}
+            >
                 <Tab.Screen
                     name="restaurants"
                     component={RestaurantsStack}
@@ -41,5 +52,38 @@ export default function Navigation() {
                 />
             </Tab.Navigator>
         </NavigationContainer>
+    );
+}
+
+function screenOptions(route, color) {
+    let iconName;
+
+    switch (route.name) {
+        case "restaurants":
+            iconName = "compass-outline";
+            break;
+        case "favorites":
+            iconName = "heart-outline";
+            break;
+        case "top-restaurants":
+            iconName = "star-outline";
+            break;
+        case "search":
+            iconName = "magnify";
+            break;
+        case "account":
+            iconName = "nintendo-game-boy";
+            break;
+
+        default:
+            break;
+    }
+    return (
+        <Icon
+            type="material-community"
+            name={iconName}
+            size={22}
+            color={color}
+        />
     );
 }
