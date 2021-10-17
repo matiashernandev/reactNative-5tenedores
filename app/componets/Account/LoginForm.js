d968db511cd7580da7e09cae69ff55693b9b3e09
nkwaaaa <nkwaaaa@gmail.com>
2021-10-17 15:56:25 -0300

    Finalizado Sistema registro y login de usuarios.

--- app/componets/Account/LoginForm.js
+++ app/componets/Account/LoginForm.js
@@ -2,12 +2,17 @@ import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import { Input, Icon, Button } from "react-native-elements";
 import { isEmpty } from "lodash";
+import { useNavigation } from "@react-navigation/native";
+import * as firebase from "firebase";
 import { validateEmail } from "../../utils/validations";
+import Loading from "../Loading";
 
 export default function LoginForm(props) {
     const { toastRef } = props;
     const [showPassword, setShowPassword] = useState(false);
     const [formData, setFormData] = useState(defaultFormValue());
+    const [loading, setLoading] = useState(false);
+    const navigation = useNavigation();
 
     const onChange = (e, type) => {
         setFormData({ ...formData, [type]: e.nativeEvent.text });
@@ -21,7 +26,19 @@ export default function LoginForm(props) {
         } else if (!validateEmail(formData.email)) {
             toastRef.current.show("El email está mal, media pila");
         } else {
-            console.log("OK MAIL");
+            setLoading(true);
+            firebase
+                .auth()
+                .signInWithEmailAndPassword(formData.email, formData.password)
+                .then(() => {
+                    setLoading(false);
+
+                    navigation.navigate("account");
+                })
+                .catch(() => {
+                    setLoading(false);
+                    toastRef.current.show("Email o contraseña MAL!!!!!!!!!");
+                });
         }
     };
 
@@ -62,6 +79,7 @@ export default function LoginForm(props) {
                 buttonStyle={styles.btnLogin}
                 onPress={onSubmit}
             />
+            <Loading isVisible={loading} text="Iniciando sesión , poné 2da" />
         </View>
     );
 }
